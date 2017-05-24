07427c28a93939ff06a253d4a746c7b20db0d300
Fred Frey <f.frey6@gmail.com>
2017-05-24 14:49:39 -0400

    Fixed clocks and byte order issue.

--- CCD_Firmware.ino
+++ CCD_Firmware.ino
@@ -20,6 +20,7 @@
 #define N05DELAY asm volatile( "nop\n" )                                               //noisey, too quick
 #define N10DELAY asm volatile( "nop\nnop\n" )                                          //still noisey
 #define N20DELAY asm volatile( "nop\nnop\nnop\nnop\n" )
+#define N25DELAY asm volatile( "nop\nnop\nnop\nnop\nnop\n" )
 #define N50DELAY asm volatile( "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n" )
 #define NXDELAY(t) { int x = ((t*100)/521 + ARM_DWT_CYCCNT); while (x > ARM_DWT_CYCCNT) {}}
 
@@ -39,6 +40,7 @@
 #define H1_HIGH GPIOD_PSOR = 0b10000
 #define FT_LOW GPIOD_PCOR = 0b1000
 #define C_LOW GPIOD_PCOR = 0b100
+#define C_HIGH GPIOD_PSOR = 0b100
 
 #define POLL_ENDRUN (GPIOA_PDIR & 0x10000)
 
@@ -53,44 +55,53 @@ const int pinSCK = 25;
 const int pinCSLD = 26;
 const int pinCLR = 27;
 
-uint16_t img[WIDTH];
+unsigned char img[WIDTH * 2];
 int lace1, lace2; // lace1 is the number of lines in the even lace, lace2 the odds
 
-comm_t comm_list[NCOMMS] = {{.name="XFRAME", .id=10},
-                      {.name="YFRAME", .id=20},
-                      {.name="CCDPX",  .id=30},
-                      {.name="CCDPY",  .id=40},
-                      {.name="CCDT",   .id=50},
-                      {.name="VER?",   .id=60},
-                      {.name="XSEC?",  .id=70},
-                      {.name="XMSEC?", .id=80},
-                      {.name="XSIZE?", .id=90},
-                      {.name="YSIZE?", .id=100},
-                      {.name="XOFFS?", .id=110},
-                      {.name="YOFFS?", .id=120},
-                      {.name="XBIN?",  .id=130},
-                      {.name="YBIN?",  .id=140},
-                      {.name="TEMP?",  .id=145},
-                      {.name="TTEMP?", .id=147},
-                      {.name="XSEC",   .id=150},
-                      {.name="XMSEC",  .id=160},
-                      {.name="XSIZE",  .id=170},
-                      {.name="YSIZE",  .id=180},
-                      {.name="XOFFS",  .id=190},
-                      {.name="YOFFS",  .id=200},
-                      {.name="XBIN",   .id=210},
-                      {.name="YBIN",   .id=220},
-                      {.name="TTEMP",  .id=225},
-                      {.name="MTEMP",  .id=227},
-                      {.name="EXPOSE", .id=230},
-                      {.name="FLUSH",  .id=240},
-                      {.name="GRIMG",  .id=250},
-                      {.name="HSHIFT", .id=255},
-                      {.name="TEST",   .id=260},
-                      {.name="THSHIFT",.id=261},
-                      {.name="TVSHIFT",.id=262},
-                      {.name="DACOFF", .id=270}
-                     };
+void expose();
+void flusher();
+void grimg();
+void tvshift();
+void hshift();
+void thshift();
+
+void dac_programmer(uint8_t chn, double val);
+
+comm_t comm_list[NCOMMS] = {{.name = "XFRAME?", .id = 10},
+  {.name = "YFRAME?", .id = 20},
+  {.name = "CCDPX?",  .id = 30},
+  {.name = "CCDPY?",  .id = 40},
+  {.name = "CCDT?",   .id = 50},
+  {.name = "VER?",   .id = 60},
+  {.name = "XSEC?",  .id = 70},
+  {.name = "XMSEC?", .id = 80},
+  {.name = "XSIZE?", .id = 90},
+  {.name = "YSIZE?", .id = 100},
+  {.name = "XOFFS?", .id = 110},
+  {.name = "YOFFS?", .id = 120},
+  {.name = "XBIN?",  .id = 130},
+  {.name = "YBIN?",  .id = 140},
+  {.name = "TEMP?",  .id = 145},
+  {.name = "TTEMP?", .id = 147},
+  {.name = "XSEC",   .id = 150},
+  {.name = "XMSEC",  .id = 160},
+  {.name = "XSIZE",  .id = 170},
+  {.name = "YSIZE",  .id = 180},
+  {.name = "XOFFS",  .id = 190},
+  {.name = "YOFFS",  .id = 200},
+  {.name = "XBIN",   .id = 210},
+  {.name = "YBIN",   .id = 220},
+  {.name = "TTEMP",  .id = 225},
+  {.name = "MTEMP",  .id = 227},
+  {.name = "EXPOSE", .id = 230},
+  {.name = "FLUSH",  .id = 240},
+  {.name = "GRIMG",  .id = 250},
+  {.name = "HSHIFT", .id = 255},
+  {.name = "TEST",   .id = 260},
+  {.name = "THSHIFT", .id = 261},
+  {.name = "TVSHIFT", .id = 262},
+  {.name = "DACOFF", .id = 270}
+};
 
 char  input_string[1024];
 char* stend = input_string;
@@ -246,7 +257,7 @@ void handle_comms() {
       grimg();
       break;
     case HSHIFT:
-      while(!POLL_ENDRUN) grimg();
+      while (!POLL_ENDRUN) grimg();
       break;
     case DACOFF:
       dac_programmer(0b11111111, 0);
@@ -270,24 +281,6 @@ void handle_comms() {
   input_string[0] = 0x00;
 }
 
-void thshift() {
-  noInterrupts();
-  H1_HIGH;
-  R_LOW;
-  while (1) {
-    HR_TOGGLE;
-    delayMicroseconds(1);
-    R_LOW;
-    delayMicroseconds(4);
-    H1_HIGH;
-    delayMicroseconds(5);
-    if (POLL_ENDRUN) {
-      interrupts();
-      Serial.write(HALT);
-      return;
-    }
-  }
-}
 
 void tvshift() {
   noInterrupts();
@@ -313,18 +306,15 @@ void tvshift() {
 }
 
 void expose() {
-  noInterrupts();
   GPIOD_PSOR |= 0b100; //v1 low
   int xtime = (xsec * 1000) + xmsec;
   while (!POLL_ENDRUN && xtime) {
     delayMicroseconds(1000);
     xtime -= 1;
   }
-  interrupts();
 }
 
 void flusher() {
-  noInterrupts();
   R_LOW;                        //initial states
   C_LOW;
   V1_LOW;
@@ -354,7 +344,7 @@ void flusher() {
   delayMicroseconds(381);          //arbitrary delay before shift
   V_TOGGLE;
   delayMicroseconds(508);
-  
+
   for (int c = 0; c < lace1; c++) {
     V_TOGGLE;                     //vertical shift
     delayMicroseconds(50);        //t_V
@@ -364,28 +354,37 @@ void flusher() {
     hshift(0);
   }
   V_TOGGLE;
-  interrupts();
 }
 
-void hshift(int flag){
-    int x = 0;
-    H1_HIGH;
-    for (int b = 0; b < WIDTH; b++) {
-      HR_TOGGLE;                  //horizontal shift
-      N50DELAY;                   //t_R
-      R_TOGGLE;                   //reset off
-      NXDELAY(500);               // + t_R = tcd
-      C_TOGGLE;
-      NXDELAY(500);               //t_C
-      C_TOGGLE;
-      H_TOGGLE;
-      NXDELAY(100);       //t_sd
-      if (flag)  img[x] = analogRead(video);
-      x++;
+void hshift(int flag) {
+  byte* img_end = img;
+  uint16_t aread;
+  C_HIGH;
+  H1_HIGH;
+  R_LOW;
+  for (int b = 0; b < WIDTH; b++) {
+    HR_TOGGLE;                  //horizontal shift
+    N50DELAY;                   //t_R
+    R_LOW;                      //reset off
+    NXDELAY(200);               // + t_R = tcd
+    C_LOW;
+    NXDELAY(500);               //t_C
+    HC_TOGGLE;
+    delayMicroseconds(1.5);       //t_sd
+    if (flag){
+        aread = analogRead(video);
+        *img_end++ = (aread >> 8);
+        *img_end++ = aread & 0xff;
+        }
     }//end row shifting
-    x = 0;
-    if (flag) Serial.write((char*)img, WIDTH * 2); // Twice the height because every pixel is a 16-bit number
-  }
+  if (flag) Serial.write((byte*)img, WIDTH * 2); // Twice the height because every pixel is a 16-bit number
+}
+
+void thshift() {
+  noInterrupts();
+  while (1) hshift(0);
+  interrupts();
+}
 
 void grimg() {
   noInterrupts();
@@ -418,13 +417,13 @@ void grimg() {
   delayMicroseconds(381);          //arbitrary delay before shift
   V_TOGGLE;
   delayMicroseconds(508);
-  
+
   for (int c = 0; c < lace1; c++) {
     V_TOGGLE;
     delayMicroseconds(5);        //t_V
     V_TOGGLE;
     hshift(1);
-    }
+  }
   V_TOGGLE;
   interrupts();
 }
@@ -476,12 +475,12 @@ void setup() {
   digitalWrite(pinCLR, HIGH);
   digitalWrite(pinSCK, LOW);
   digitalWrite(pinCSLD, HIGH);
-  
+
   analogReadResolution(12);
   analogWriteResolution(12);
 
   analogWrite(A3, 1023);
-  
+
   GPIOB_PDOR = 1; // Set ports B-E to outputs.
   GPIOC_PDOR = 1;
   GPIOD_PDOR = 1;
@@ -489,11 +488,10 @@ void setup() {
 
   // Setup operating variables.
   xsize = WIDTH; ysize = HEIGHT;
-  lace1 = HEIGHT / 2; lace2 = (HEIGHT / 2) + 1;
-  
+  lace1 = 247; lace2 = 248;
+
   xoffs = yoffs = 0;
-  xsec = 30;
-  xmsec = 0;
+  xsec = xmsec = 0;
   xbin = ybin = 1;
 
   ARM_DEMCR |= ARM_DEMCR_TRCENA; //CPU cycles, might not be necessary
@@ -518,20 +516,20 @@ void setup() {
 
 }
 
-float get_temp(){
+float get_temp() {
   float temp = analogRead(A0);
   return temp;
-  }
+}
 
 // Set the duty cycle for the PEC, where duty is out of 1023
-void set_temp(int duty){
+void set_temp(int duty) {
   analogWrite(A3, 1023 - duty);
   pec_duty = duty;
-  }
+}
 
 void loop() {
-  while(1){
-    if(temp_mon) Serial.println(get_temp());
+  while (1) {
+    if (temp_mon) Serial.println(get_temp());
     yield();
     delay(5);
   }
